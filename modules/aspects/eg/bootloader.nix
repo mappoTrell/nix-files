a0b42292f409b48f792da6a9b02a83fbeee30386
xelix <xeliix@proton.me>
2026-02-17 00:48:24 +0100

    some changes

---
+++ modules/aspects/eg/bootloader.nix
@@ -0,0 +1,8 @@
+{eg, ...}: {
+  eg.bootloader.nixos = {}: {
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.systemd-boot.memtest86.enable = true;
+    boot.binfmt.emulatedSystems = ["aarch64-linux" "riscv64-linux"];
+  };
+}
---
+++ modules/aspects/nixos/defaults.nix
@@ -0,0 +1,26 @@
+{
+  den,
+  eg,
+  inputs,
+  __findFile,
+  ...
+}: {
+  den.aspects.nixos = {aspects, ...}: {
+    # igloo host provides some home-manager defaults to its users.
+    homeManager.programs.direnv.enable = true;
+
+    includes = [
+      eg.nividia
+      # eg.laptop
+    ];
+
+    nixos = {pkgs, ...}: {
+      environment.systemPackages = [pkgs.hello];
+    };
+
+    # <host>.provides.<user>, via eg/routes.nix
+    provides.xelix = {user, ...}: {
+      homeManager.programs.helix.enable = user.name == "xelix";
+    };
+  };
+}
