ba47c01d1524fdf79ac3376a5cd3803d48724787
xelix <xeliix@proton.me>
2026-04-21 11:09:20 +0200

    old den version

--- modules/dendritic.nix
+++ modules/dendritic.nix
@@ -4,7 +4,7 @@
   ...
 }: {
   flake-file.inputs.flake-file.url = lib.mkDefault "github:vic/flake-file";
-  flake-file.inputs.den.url = lib.mkDefault "github:vic/den";
+  flake-file.inputs.den.url = lib.mkDefault "github:vic/den/latest";
   imports = [
     (inputs.flake-file.flakeModules.dendritic or {})
     (inputs.den.flakeModules.dendritic or {})
